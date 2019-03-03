7ec1964b3f6be05966e83d01a4faa7c2805d062b
Eryk <chiracle@gmail.com>
2019-03-03 23:32:19 +0100

    Added news section

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-$(window).load(function(){
-    var $container = $('.works__portfolioContainer');
+$(window).load(function() {
+  var $container = $(".works__portfolioContainer");
+  $container.isotope({
+    // layoutMode: "fitRows",
+    // layoutMode: "horiz",
+    resize: false,
+    filter: "*",
+    animationOptions: {
+      duration: 750,
+      easing: "linear",
+      queue: false
+    }
+  });
+
+  $(".works__portfolioFilter a").click(function() {
+    $(".works__portfolioFilter .current").removeClass("current");
+    $(this).addClass("current");
+
+    var selector = $(this).attr("data-filter");
     $container.isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-        }
+      resize: false,
+      filter: selector,
+      animationOptions: {
+        duration: 750,
+        easing: "linear",
+        queue: false
+      }
     });
- 
-    $('.works__portfolioFilter a').click(function(){
-        $('.works__portfolioFilter .current').removeClass('current');
-        $(this).addClass('current');
- 
-        var selector = $(this).attr('data-filter');
-        $container.isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-         });
-         return false;
-    }); 
+    return false;
+  });
 });
-    
-    
-    
-    
