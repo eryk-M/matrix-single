a2b64221476a17a6afd3329d669e021ce4c6cfb2
Eryk <chiracle@gmail.com>
2019-03-03 20:14:45 +0100

    Recent works section added, script isotope

---
+++ main.js
@@ -0,0 +1,31 @@
+$(window).load(function(){
+    var $container = $('.works__portfolioContainer');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+ 
+    $('.works__portfolioFilter a').click(function(){
+        $('.works__portfolioFilter .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    }); 
+});
+    
+    
+    
+    
