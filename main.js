bb0cc2d390fc051150c5dc913ca98ee50d81250f
Eryk <chiracle@gmail.com>
2019-03-05 19:06:42 +0100

    Responsive #3

--- main.js
+++ main.js
@@ -1,30 +1,9 @@
 /*      PORTFOLIO      */
-window.onload = () => {
-  var $container = $(".works__portfolioContainer");
-  $container.isotope({
-    resize: false,
-    filter: "*",
-    animationOptions: {
-      duration: 750,
-      easing: "linear",
-      queue: false
-    }
-  });
-
-  $(".works__portfolioFilter a").click(function() {
-    $(".works__portfolioFilter .current").removeClass("current");
+$(document).ready(function() {
+  // Start mixitup
+  $("#mix-container").mixItUp();
+  $(".controls button").click(function() {
+    $(".controls .current").removeClass("current");
     $(this).addClass("current");
-
-    var selector = $(this).attr("data-filter");
-    $container.isotope({
-      resize: false,
-      filter: selector,
-      animationOptions: {
-        duration: 750,
-        easing: "linear",
-        queue: false
-      }
-    });
-    return false;
   });
-};
+});
